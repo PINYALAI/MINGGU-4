33cef1a380a51c12ba75a32d961aa4bab806a2bc
PINYALAI <alfurqonikazd887@gmail.com>
2026-03-10 23:31:27 +0700

    status nilai

--- pti1.cpp
+++ pti1.cpp
@@ -18,3 +18,17 @@ float hitungrata(float a, float b, float c)
 {
     return (a + b + c) / 3;
 }
+
+string statusnilai(float rata)
+{
+    if (rata >= 85)
+      return "Grade A";
+    else if (rata >= 75)
+        return "Grade B";
+    else if (rata >= 65)
+        return "Grade C";
+    else if (rata >= 50)
+        return "Grade D";
+    else
+        return "Grade E";
+}
