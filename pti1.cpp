92f0d0ef72717aee2cdfde6db4695b25f622bdde
PINYALAI <alfurqonikazd887@gmail.com>
2026-03-10 23:28:59 +0700

    hitungrata

--- pti1.cpp
+++ pti1.cpp
@@ -6,5 +6,15 @@ float nilai1, nilai2, nilai3;
 void inputnilai()
 {
     cout << "\n--- Program Penilaian Mahasiswa ---" << endl;
-    
+    cout << "Masukkan Nilai Tugas  : ";
+    cin >> nilai1;
+    cout << "Masukkan Nilai UTS    : ";
+    cin >> nilai2;
+    cout << "Masukkan Nilai UAS    : ";
+    cin >> nilai3;
+}
+
+float hitungrata(float a, float b, float c)
+{
+    return (a + b + c) / 3;
 }
